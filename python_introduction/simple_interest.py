578a845df3390b54d68973b759c3a5a8bb25e4b2
Joseph Ndayizeye <josephndayizeye250@gmail.com>
2025-09-10 23:28:23 +0200

    modified variables

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,10 +1,10 @@
 #principal with a value
-P = 1000
+principal = 1000
 #rate
-R = 0.05
+rate = 0.05
 #time in years
-T = 3
+time = 3
 #simple interest
-interest = P*R*T
+interest = principal*rate*time
 #final results
 print(f"The simple interest is {interest}")
