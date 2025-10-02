8131789c14ae05c9a36bc639556d395e2455cb16
Joseph Ndayizeye <josephndayizeye250@gmail.com>
2025-10-02 21:24:16 +0200

    added simple bank account class program

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,19 @@
+class BankAccount:
+    def __init__(self, account_balance = 0):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        self.account_balance += amount
+        return self.account_balance
+
+
+    def withdraw(self, amount):
+        if self.account_balance>=amount:
+            self.account_balance -= amount
+            return self.account_balance
+        else:
+            print("Insuficient funds.")
+
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
