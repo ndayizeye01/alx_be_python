c869da1ba28bb143aac3f11344c92e42453a1f08
Joseph Ndayizeye <josephndayizeye250@gmail.com>
2025-10-02 21:32:06 +0200

    added simple bank account class program

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -16,4 +16,4 @@ class BankAccount:
 
 
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${float(self.account_balance, 2)}")
