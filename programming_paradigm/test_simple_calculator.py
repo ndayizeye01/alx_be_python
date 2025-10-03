3ab389496983041194b8fad2cebe6c5fb410859b
Joseph Ndayizeye <josephndayizeye250@gmail.com>
2025-10-03 09:19:29 +0200

    Added testing program for simple calculator.

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -5,25 +5,25 @@ from simple_calculator import SimpleCalculator
 class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         """Set up the SimpleCalculator instance before each test."""
-        self.SimpleCalculator = SimpleCalculator()
+        self.calc = SimpleCalculator()
 
     def test_addition(self):
-        results = self.SimpleCalculator.add(5,3)
+        results = self.calc.add(5,3)
         self.assertEqual(results,8)
 
     def test_subtraction(self):
-        results = self.SimpleCalculator.subtract(5,3)
+        results = self.calc.subtract(5,3)
         self.assertEqual(results,2)
 
     def test_multiplication(self):
-        results = self.SimpleCalculator.multiply(5,3)
+        results = self.calc.multiply(5,3)
         self.assertEqual(results,15)
 
     def test_division(self):
-        results = self.SimpleCalculator.divide(15,3)
+        results = self.calc.divide(15,3)
         self.assertEqual(results,5)
         with self.assertRaises(ZeroDivisionError):
-            self.SimpleCalculator.divide(10, 0)
+            self.calc.divide(10, 0)
 
 
 
