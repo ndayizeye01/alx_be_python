3c1acd51f6507c13e05b2222c189430679ee8d69
Joseph Ndayizeye <josephndayizeye250@gmail.com>
2025-10-14 20:10:31 +0200

    added polymorphism demo program

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,25 @@
+import math
+
+
+class Shape:
+    def __init__(self):
+        pass
+    def area(self):
+        raise NotImplementedError
+
+class Rectangle(Shape):
+    def __init__(self, length: float, width: float):
+        super().__init__()
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.width*self.length
+    
+class Circle(Shape):
+    def __init__(self, radius: float):
+        super().__init__()
+        self.radius = radius
+
+    def area(self):
+        return math.pi*self.radius*self.radius
